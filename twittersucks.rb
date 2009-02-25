bf95ecad12555bbc30b6ebce2141f6a113e555c7
Grant Hutchins <nertzy@gmail.com>
2009-02-25 01:04:58 -0600

    Working page that fetches and displays tweets with the search string '"twitter sucks" OR twittersucks'

--- twittersucks.rb
+++ twittersucks.rb
@@ -1,6 +1,15 @@
 require 'rubygems'
 require 'sinatra'
+require 'twitter'
+require 'action_view'
+
+helpers do
+  include Rack::Utils
+  include ActionView::Helpers::DateHelper
+  alias_method :h, :escape_html
+end
 
 get '/' do
-  'Twitter sucks!'
+  @search = Twitter::Search.new('"twitter sucks" OR twittersucks').to_a
+  erb :index
 end
